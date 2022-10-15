a8b9ca8b41a64bd2412f06f25f39011a659756fa
Lautaro <lautaroef@gmail.com>
2022-10-15 14:50:15 -0300

    Refactored to TypeScript

--- client/src/components/other-components/Visibility.js
+++
@@ -1,6 +0,0 @@
-export const VisibilityOn = () => (
-  <i className='fas fa-eye-slash' style={{ fontSize: '1.35rem' }} />
-);
-export const VisibilityOff = () => (
-  <i className='fas fa-eye' style={{ fontSize: '1.35rem' }} />
-);
